c632aca0bf8b075c8a24c1d66e11baf43710f75f
namant <namant@iitbhilai.ac.in>
2026-03-26 01:06:46 +0530

    feat: Add background animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navigation } from './core/navigation/navigation';
 import { Footer } from './core/footer/footer';
+import { BackgroundAnimation } from './core/background-animation/background-animation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navigation, Footer],
+  imports: [RouterOutlet, Navigation, Footer, BackgroundAnimation],
   templateUrl: './app.html'
 })
 export class App { }
---
+++ src/app/core/background-animation/background-animation.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-background-animation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './background-animation.html'
+})
+export class BackgroundAnimation {}
