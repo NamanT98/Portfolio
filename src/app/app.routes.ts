067c177c3c757dfe69d47e3b171a73f2cd24ca1d
namant <namant@iitbhilai.ac.in>
2026-03-26 00:04:37 +0530

    feat: Incorporating feedbacks changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Main } from './features/main/main';
+import { AllProjects } from './features/all-projects/all-projects';
 
 export const routes: Routes = [
   { path: '', component: Main },
+  { path: 'projects', component: AllProjects },
   { path: '**', redirectTo: '' }
 ];
